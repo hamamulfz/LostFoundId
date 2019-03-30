0b52b5b96fa7706580726aa2b6d168081a9776d0
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2019-03-30 19:39:30 +0700

    with dicoding template edited with db information edited

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     $db = "Registration";
 
     try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn = new PDO("sqlsrv:server = tcp:lostfoundidserver.database.windows.net,1433; Database = LostDounfDb", "hfauzy96", "Just4fun");
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
