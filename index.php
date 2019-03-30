aac26657d69acaaeb5616daf6b910372d80c1b98
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2019-03-30 19:33:38 +0700

    with dicoding template edited with db information

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "lostfoundidserver.database.windows.net";
+    $user = "hfauzy96";
+    $pass = "Just4fun";
+    $db = "Registration";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
